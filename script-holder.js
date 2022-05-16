16a653b8bbf221dcd1491a8f5b6044b1aa772f27
Samantha-Ruth <sam.haberman@gmail.com>
2022-05-15 21:48:46 -0600

    final horrible coding

--- script-holder.js
+++ script-holder.js
@@ -4,120 +4,84 @@
 var startButtonEl = document.querySelector("#quiz-start");
 startButtonEl.addEventListener("click", function() {
     // Timer Elements
-var timerEl = document.getElementById('countdown');
+// var timerEl = document.getElementById('countdown');
 
-// Starts a Timer that counts down from 30
-var timeLeft = 30;
-var savedTime = [];
+// // Starts a Timer that counts down from 30
+// var timeLeft = 30;
+// var savedTime = [];
 
-function countdown() {
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = "Time: " + timeLeft;
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '' + "Time's Up!";
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `endQuiz()` function
-        endQuiz();
-      }
-    }, 1000);
- }
-countdown();
-    //End Quiz when time runs out
-function endQuiz () {
-        var newQuestionEl = document.querySelector("#quiz-question")
-        newQuestionEl.textContent = "All Done!"
-        // Enter High Scores
-        var newQuestionEl = document.querySelector("#quiz-question")
-        newQuestionEl.textContent = "All Done!"
-        var highScoresForm = createElement("form");
-        highScoresForm.innerHTML = "<label for='name'>Enter your initials:</label><input type='text'name='initials'id='initials'class='form-input'/><button type='submit'>Submit</button>";
-        newQuestionEl.appendChild(highScoresForm);
+// var timer = null;
 
-        scores[i].setAttribute("", scoreIdCounter)
-        // Save High Scores
-        var scores = [];
-        var highScoresObj = {name: highScoreInput, score: savedTime};
-        scoresObj.id=scoreIdCounter
-        scores.push(highScoresObj);
-        // save tasks to localStorage
-        savedScores();
-        // increase task counter for next unique score id
-        scoreIdCounter++;
 
-        var savedScores = function() {
-            localStorage.setItem("scores", JSON.stringify(scores));
-        }
+// function countdown() {
+//     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//     var timeInterval = setInterval(function () {
+//       // As long as the `timeLeft` is greater than 1
+//       if (timeLeft > 1) {
+//         // Set the `textContent` of `timerEl` to show the remaining seconds
+//         timerEl.textContent = "Time: " + timeLeft;
+//         // Decrement `timeLeft` by 1
+//         timeLeft--;
+//       } else {
+//         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+//         timerEl.textContent = '' + "Time's Up!";
+//         // Use `clearInterval()` to stop the timer
+//         clearInterval(timeInterval);
+//         // Call the `endQuiz()` function
+//         endQuiz();
+//       }
+//     }, 1000);
+// }
+// countdown();
 
-        // Print High Scores
-        var loadScores = function() {
-            // assign score variable
-            var savedScores = localStorage.getItem("scores");
-            // if scores = null set scores back to an empty array
-            if (!savedScores) {
-                return false}
-            }
-        // retrieve scores from local storage
-        scores = JSON.parse(scores);
-
-        //print items to page
-        var newQuestionEl = document.querySelector("#quiz-question")
-        newQuestionEl.textContent = ""
-        var printHighScoresEl = createElement("ol");
-        var printHighScoresListEl = createElement("li");
-        printHighScoresListEl.setAttribute("class=print-high-scores");
-        scores[i].setAttribute(loadScores,scoreIdCounter);
-        newQuestionEl.appendChild(printHighScoresEl);
-        printHighScoresEl.appendChild(printHighScoresListEl);
-        var resetReturnButtonsEl = createElement("div");
-        resetReturnButtonsEl.getAttribute("class=high-scores");
-        resetReturnButtonsEl.innerHTML("<button>Go Back</button><button type='reset'>Clear High Scores</button>");
-    }
-newAnswer1AEl.innerHTML = "<button id='answer-one-a answer'>1. Strings</button>"
-// Append new ordered list (#quiz-answers) and list with button (.quiz-answer-option) to the div
-newQuestionEl.appendChild(newAnswer1AEl);
+    // End Quiz when time runs out
+    // function endQuiz () {
+    //     var newQuestionEl = document.querySelector("#quiz-question")
+    //     newQuestionEl.textContent = "All Done!"
+    //     // remove other text boxes
+    //     //findCorrectElToRemove.remove();  ***  THESE ARENT THE CORRECT ELEMENTS TO REMOVE!  
+    //     // newAnswerAEl.remove();
+    //     // newAnswerBEl.remove();
+    //     // newAnswerCEl.remove();
+    //     // newAnswerDEl.remove();
+    //     // Final Score is 
+    //     // Enter initials , input area, submit button
+    // }
     
-    // To decrease time by 10 seconds with a wrong answer 
-    function subtractTen() {
-        timeLeft= (timeLeft- 10);
-    }
+    // // To decrease time by 10 seconds with a wrong answer 
+    // function subtractTen() {
+    //     timeLeft= (timeLeft- 10);
+    // }
 
-    // To stop the clock when user reaches the end of the questions. 
-    function stopClock() {
-            var saveTime = timeLeft; 
-            savedTime.push(saveTime);//save this time to storage
-            timeLeft = 0;
-            console.log(saveTime);
-        }
-        console.log(savedTime);
+    // // To stop the clock when user reaches the end of the questions. 
+    // function stopClock() {
+    //         var saveTime = timeLeft; 
+    //         savedTime.push(saveTime);//save this time to storage
+    //         timeLeft = 0;
+    //         console.log(saveTime);
+    //     }
+    //     console.log(savedTime);
  
 // ** Start of quiz ** 
     // find quiz intro
-    var quizQuestionsEl = document.querySelector(".quiz-topper");
-    var quizIntroEl = document.querySelector(".intro")
+    var quizQuestions = document.querySelector(".quiz-topper");
+    var quizIntro = quizQuestions.querySelector(".intro")
     // add an id to each new element? then can delete it in the future? 
     // create new header
     var newQuestionEl = document.createElement("h1");
     newQuestionEl.textContent = "Commonly used datatypes do NOT include:";
     newQuestionEl.setAttribute("id", "quiz-question");
-    quizQuestionsEl.appendChild(newQuestionEl);
+    quizQuestions.appendChild(newQuestionEl);
     // remove old header/intro
-    quizIntroEl.remove();
+    quizIntro.remove();
 
     // Target Intro paragraph and Start Button
-    var startingParagraphEl = document.querySelector(".quiz-body");
+    var startingParagraph = document.querySelector(".quiz-body");
     // remove paragraph
-    startingParagraphEl.remove(); 
-    var startButtonEl = document.querySelector(".quiz-start");
+    startingParagraph.remove(); 
+    var startButton = document.querySelector(".quiz-start");
     // Remove Start Button
-    startButtonEl.remove();
+    startButton.remove();
 
 // Target quiz area. Add an ordered list.
     var quizAnswers1El = document.querySelector("#quiz-answer-wrapper");
@@ -127,11 +91,11 @@ newQuestionEl.appendChild(newAnswer1AEl);
     quizAnswers1El.appendChild(newQuestionEl);
 
 // Add List Element to Ordered List, with buttons inside each list element
-    var newAnswer1AEl = document.createElement("li");
-    newAnswer1AEl.setAttribute("class", "quiz-answer-option")
-    newAnswer1AEl.innerHTML = "<button id='answer-one-a answer'>1. Strings</button>"
+    var newAnswerEl = document.createElement("li");
+    newAnswerEl.setAttribute("class", "quiz-answer-option")
+    newAnswerEl.innerHTML = "<div id='answer-one-a answer'>1. Strings</div>"
     // Append new ordered list (#quiz-answers) and list with button (.quiz-answer-option) to the div
-    newQuestionEl.appendChild(newAnswer1AEl);
+    newQuestionEl.appendChild(newAnswerEl);
         //Second Answer
     var quizAnswer1BEl = document.querySelector("#quiz-answers");
     var newAnswer1BEl = document.createElement("li");
@@ -156,25 +120,40 @@ newQuestionEl.appendChild(newAnswer1AEl);
 
 
     // Put listener on Answer Buttons and replace with new question and answer options
-    var selectedAnswer1AEl = document.querySelector("#answer-one-a");
-    var correctAnswer1BEl = document.querySelector("#answer-one-b");
-    var correctAnswer1CEl = document.querySelector("#answer-one-c");
-    var correctAnswer1DEl = document.querySelector("#answer-one-d");
+    var correctAnswer1AEl = document.querySelector("#answer-one")
 
     if (correctAnswer1AEl.addEventListener("click", function() {
     console.log("Wrong!");
     newQuestionEl.textContent = "The condition in an 'if/then' statement is enclosed with:";
     newQuestionEl.setAttribute("id", "quiz-question");
-    newQuestionEl.setAttribute("class", "quiz-answer-option")
-    newQuestionEl.innerHTML = "<button id='answer-one'>1. Quotes</button>"
-    newAnswer1BEl.setAttribute("class", "quiz-answer-option");
-    newAnswer1BEl.innerHTML = "<button id='answer-two'>2. Curly Brackets</button>"
-    newAnswer1CEl.setAttribute("class", "quiz-answer-option");
-    newAnswer1CEl.innerHTML = "<button id='answer-three'>3. Square Brackets</button>"
-    newAnswer1DEl.setAttribute("class", "quiz-answer-option");
-    newAnswer1DEl.innerHTML = "<button id='answer-four'>4. Parentheses</button>"
+    newAnswerListEl.setAttribute("class", "quiz-answer-option")
+    newAnswerListEl.innerHTML = "<button id='answer-one'>1. Quotes</button>"
+    newAnswerBEl.setAttribute("class", "quiz-answer-option");
+    newAnswerBEl.innerHTML = "<button id='answer-two'>2. Curly Brackets</button>"
+    newAnswerCEl.setAttribute("class", "quiz-answer-option");
+    newAnswerCEl.innerHTML = "<button id='answer-three'>3. Square Brackets</button>"
+    newAnswerDEl.setAttribute("class", "quiz-answer-option");
+    newAnswerDEl.innerHTML = "<button id='answer-four'>4. Parentheses</button>"
+     
     }));
 });
+    // var correctAnswer1BEl = document.querySelector("#answer-one-b");
+    // var correctAnswer1CEl = document.querySelector("#answer-one-c");
+    // var correctAnswer1DEl = document.querySelector("#answer-one-d");
+
+    // if (correctAnswer1AEl.addEventListener("click", function() {
+    // console.log("Wrong!");
+    // newQuestionEl.textContent = "The condition in an 'if/then' statement is enclosed with:";
+    // newQuestionEl.setAttribute("id", "quiz-question");
+    // newQuestionEl.setAttribute("class", "quiz-answer-option")
+    // newQuestionEl.innerHTML = "<button id='answer-one'>1. Quotes</button>"
+    // newAnswer1BEl.setAttribute("class", "quiz-answer-option");
+    // newAnswer1BEl.innerHTML = "<button id='answer-two'>2. Curly Brackets</button>"
+    // newAnswer1CEl.setAttribute("class", "quiz-answer-option");
+    // newAnswer1CEl.innerHTML = "<button id='answer-three'>3. Square Brackets</button>"
+    // newAnswer1DEl.setAttribute("class", "quiz-answer-option");
+    // newAnswer1DEl.innerHTML = "<button id='answer-four'>4. Parentheses</button>"
+    // }));
 //     if (correctAnswer1BEl.addEventListener("click", function() {
 //         console.log("Wrong!");
 //         newQuestionEl.textContent = "The condition in an 'if/then' statement is enclosed with:";
